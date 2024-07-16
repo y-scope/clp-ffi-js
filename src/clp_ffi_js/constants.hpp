4e04f2bb0a14022d03cb693ff1beadc1df1cded5
Junhao Liao <junhao@junhao.ca>
2024-07-16 18:27:41 -0400

    Remove `sv` literals from cLogLevelNames.

--- src/clp_ffi_js/constants.hpp
+++ src/clp_ffi_js/constants.hpp
@@ -4,16 +4,14 @@
 #include <array>
 
 namespace clp_ffi_js {
-using namespace std::literals::string_view_literals;
-
 constexpr std::array cLogLevelNames{
-        "NONE"sv,  // This should not be used.
-        "TRACE"sv,
-        "DEBUG"sv,
-        "INFO"sv,
-        "WARN"sv,
-        "ERROR"sv,
-        "FATAL"sv,
+        "NONE",  // This should not be used.
+        "TRACE",
+        "DEBUG",
+        "INFO",
+        "WARN",
+        "ERROR",
+        "FATAL",
 };
 }  // namespace clp_ffi_js
 
