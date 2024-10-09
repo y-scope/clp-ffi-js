bd0371a19fd4a5420d6f62b7e0f2b25500a0c0cd
Dave Marco <marcovitch.david@gmail.com>
2024-10-09 23:18:33 +0000

    inline

--- src/clp_ffi_js/ir/LogEventWithLevel.hpp
+++ src/clp_ffi_js/ir/LogEventWithLevel.hpp
@@ -32,17 +32,13 @@ public:
             : clp::ir::LogEvent<encoded_variable_t>{timestamp, utc_offset, std::move(message)},
               m_log_level{log_level} {}
 
-    // Methods
-    [[nodiscard]] auto get_log_level() const -> LogLevel;
+    [[nodiscard]] auto get_log_level() const -> LogLevel {
+        return m_log_level;
+    }
 
 private:
     LogLevel m_log_level;
 };
-
-template <typename encoded_variable_t>
-auto LogEventWithLevel<encoded_variable_t>::get_log_level() const -> LogLevel {
-    return m_log_level;
-}
 }  // namespace clp_ffi_js::ir
 
 #endif  // CLP_FFI_JS_IR_LOGEVENTWITHLEVEL_HPP
