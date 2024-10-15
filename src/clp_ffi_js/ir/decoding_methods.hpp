15d818ce37b5f1ea10bd6505e1935913b2c53fb3
Junhao Liao <junhao@junhao.ca>
2024-10-14 21:41:15 -0400

    Add docs for `rewind_reader_and_verify_encoding_type`.

--- src/clp_ffi_js/ir/decoding_methods.hpp
+++ src/clp_ffi_js/ir/decoding_methods.hpp
@@ -4,6 +4,12 @@
 #include <clp/ReaderInterface.hpp>
 
 namespace clp_ffi_js::ir {
+/**
+ * Rewinds the reader to the beginning and verifies the CLP IR data encoding type.
+ * @param reader
+ * @throws ClpFfiJsException if there is a failure to decode the encoding type or if the encoding
+ * type is unsupported.
+ */
 auto rewind_reader_and_verify_encoding_type(clp::ReaderInterface& reader) -> void;
 }  // namespace clp_ffi_js::ir
 
