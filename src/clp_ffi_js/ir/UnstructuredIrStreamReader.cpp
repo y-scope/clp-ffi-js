e5a8e4c0435e0c46c000df4db31c4687ec0075ad
Dave Marco <marcovitch.david@gmail.com>
2024-11-09 22:41:54 +0000

    fix lint

--- src/clp_ffi_js/ir/UnstructuredIrStreamReader.cpp
+++ src/clp_ffi_js/ir/UnstructuredIrStreamReader.cpp
@@ -19,14 +19,13 @@
 #include <clp/ir/types.hpp>
 #include <clp/TraceableException.hpp>
 #include <clp/type_utils.hpp>
-
 #include <emscripten/bind.h>
 #include <emscripten/em_asm.h>
 #include <emscripten/val.h>
 #include <spdlog/spdlog.h>
 
-#include <clp_ffi_js/constants.hpp>
 #include <clp_ffi_js/ClpFfiJsException.hpp>
+#include <clp_ffi_js/constants.hpp>
 #include <clp_ffi_js/ir/StreamReader.hpp>
 #include <clp_ffi_js/ir/StreamReaderDataContext.hpp>
 
