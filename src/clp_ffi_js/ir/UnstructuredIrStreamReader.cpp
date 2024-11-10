8ae1fa2965f2ec0d267b5fe34f0c5a1e597e509d
Dave Marco <marcovitch.david@gmail.com>
2024-11-10 01:20:33 +0000

    attempt to fix lint

--- src/clp_ffi_js/ir/UnstructuredIrStreamReader.cpp
+++ src/clp_ffi_js/ir/UnstructuredIrStreamReader.cpp
@@ -25,6 +25,7 @@
 #include <spdlog/spdlog.h>
 
 #include <clp_ffi_js/ClpFfiJsException.hpp>
+#include <clp_ffi_js/constants.hpp>
 #include <clp_ffi_js/ir/LogEventWithFilterData.hpp>
 #include <clp_ffi_js/ir/StreamReader.hpp>
 #include <clp_ffi_js/ir/StreamReaderDataContext.hpp>
