de5aaf596185e3ca3bdb5bde5f1c58f41f0783f8
Dave Marco <marcovitch.david@gmail.com>
2024-11-11 00:06:37 +0000

    add new functions

---
+++ src/clp_ffi_js/ir/StructuredIrUnitHandler.cpp
@@ -0,0 +1,148 @@
+#include "StructuredIrUnitHandler.hpp"
+
+#include <cstddef>
+#include <format>
+#include <memory>
+#include <string>
+#include <string_view>
+#include <type_utils.hpp>
+
+#include <clp/Array.hpp>
+#include <clp/ErrorCode.hpp>
+#include <clp/ffi/ir_stream/Deserializer.hpp>
+#include <clp/ffi/KeyValuePairLogEvent.hpp>
+#include <clp/ffi/Value.hpp>
+#include <clp/ir/types.hpp>
+#include <clp/TraceableException.hpp>
+#include <emscripten/em_asm.h>
+#include <emscripten/val.h>
+#include <spdlog/spdlog.h>
+
+#include <clp_ffi_js/ClpFfiJsException.hpp>
+#include <clp_ffi_js/constants.hpp>
+#include <clp_ffi_js/ir/LogEventWithFilterData.hpp>
+#include <clp_ffi_js/ir/StreamReader.hpp>
+#include <clp_ffi_js/ir/StreamReaderDataContext.hpp>
+
+namespace clp_ffi_js::ir {
+
+namespace {
+/**
+ * Parses the `LogLevel` from an input string.
+ * @param str
+ * @return
+ */
+auto parse_log_level(std::string_view str) -> LogLevel;
+
+auto parse_log_level(std::string_view str) -> LogLevel {
+    LogLevel log_level{LogLevel::NONE};
+
+    // Convert the string to uppercase,
+    std::string log_level_name_upper_case{str};
+    std::transform(
+            log_level_name_upper_case.begin(),
+            log_level_name_upper_case.end(),
+            log_level_name_upper_case.begin(),
+            [](unsigned char c) { return std::toupper(c); }
+    );
+
+    // Do not accept "None" when checking if input string is in `cLogLevelNames`.
+    auto const* it = std::ranges::find(
+            cLogLevelNames.begin() + clp::enum_to_underlying_type(cValidLogLevelsBeginIdx),
+            cLogLevelNames.end(),
+            log_level_name_upper_case
+    );
+
+    if (it == cLogLevelNames.end()) {
+        return log_level;
+    }
+
+    return static_cast<LogLevel>(std::distance(cLogLevelNames.begin(), it));
+}
+} // namespace
+
+using clp::ir::four_byte_encoded_variable_t;
+auto IrUnitHandler::handle_schema_tree_node_insertion(
+        clp::ffi::SchemaTree::NodeLocator schema_tree_node_locator
+) -> clp::ffi::ir_stream::IRErrorCode {
+    ++m_current_node_id;
+
+    auto const& key_name{schema_tree_node_locator.get_key_name()};
+    if (m_log_level_key == key_name) {
+        m_log_level_node_id.emplace(m_current_node_id);
+    } else if (m_timestamp_key == key_name) {
+        m_timestamp_node_id.emplace(m_current_node_id);
+    }
+
+    return clp::ffi::ir_stream::IRErrorCode::IRErrorCode_Success;
+}
+
+auto IrUnitHandler::handle_log_event(StructuredLogEvent&& log_event
+) -> clp::ffi::ir_stream::IRErrorCode {
+    auto const& id_value_pairs{log_event.get_node_id_value_pairs()};
+    clp::ffi::value_int_t timestamp = get_timestamp(id_value_pairs);
+     LogLevel log_level = get_log_level(id_value_pairs);
+
+    auto log_event_with_filter_data{
+            LogEventWithFilterData<StructuredLogEvent>(std::move(log_event), log_level, timestamp)
+    };
+
+    m_deserialized_log_events->emplace_back(std::move(log_event_with_filter_data));
+
+    return clp::ffi::ir_stream::IRErrorCode::IRErrorCode_Success;
+}
+
+auto IrUnitHandler::get_log_level(StructuredLogEvent::NodeIdValuePairs const& id_value_pairs) const -> LogLevel {
+    LogLevel log_level{LogLevel::NONE};
+
+    if (false == m_log_level_node_id.has_value()) {
+        return log_level;
+    }
+
+    auto const& log_level_pair{id_value_pairs.at(m_log_level_node_id.value())};
+
+    if (false == log_level_pair.has_value()) {
+        return log_level;
+    }
+
+    if (log_level_pair->is<std::string>()) {
+        auto const& log_level_node_value
+                = log_level_pair.value().get_immutable_view<std::string>();
+        log_level = parse_log_level(log_level_node_value);
+    } else if (log_level_pair->is<clp::ffi::value_int_t>()) {
+        auto const& log_level_node_value = (log_level_pair.value().get_immutable_view<clp::ffi::value_int_t>());
+        if (log_level_node_value <= (clp::enum_to_underlying_type(cValidLogLevelsEndIdx))) {
+            log_level = static_cast<LogLevel>(log_level_node_value);
+        }
+    } else {
+        auto log_event_idx = m_deserialized_log_events->size();
+        SPDLOG_ERROR("Log level type is not int or string for log event index {}", log_event_idx);
+    }
+
+    return log_level;
+
+}
+
+auto IrUnitHandler::get_timestamp(StructuredLogEvent::NodeIdValuePairs const& id_value_pairs) const -> clp::ffi::value_int_t {
+    clp::ffi::value_int_t timestamp{0};
+
+    if (false == m_timestamp_node_id.has_value()) {
+        return timestamp;
+    }
+
+    auto const& timestamp_pair{id_value_pairs.at(m_timestamp_node_id.value())};
+
+    if (false == timestamp_pair.has_value()) {
+        return timestamp;
+    }
+
+    if (timestamp_pair->is<clp::ffi::value_int_t>()) {
+        timestamp = timestamp_pair.value().get_immutable_view<clp::ffi::value_int_t>();
+    } else {
+        // TODO: Add support for parsing timestamp values of string type.
+        auto log_event_idx = m_deserialized_log_events->size();
+    }
+
+    return timestamp;
+}
+}  // namespace clp_ffi_js::ir
---
+++ src/clp_ffi_js/ir/StructuredIrUnitHandler.hpp
@@ -0,0 +1,130 @@
+#ifndef CLP_FFI_JS_IR_STRUCTUREDIRUNITHANDLER_HPP
+#define CLP_FFI_JS_IR_STRUCTUREDIRUNITHANDLER_HPP
+
+#include <cstddef>
+#include <memory>
+#include <optional>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include <clp/Array.hpp>
+#include <clp/ffi/ir_stream/decoding_methods.hpp>
+#include <clp/ffi/ir_stream/Deserializer.hpp>
+#include <clp/ffi/KeyValuePairLogEvent.hpp>
+#include <clp/ffi/SchemaTree.hpp>
+#include <clp/time_types.hpp>
+#include <emscripten/val.h>
+#include <spdlog/spdlog.h>
+
+#include <clp_ffi_js/ir/LogEventWithFilterData.hpp>
+#include <clp_ffi_js/ir/StreamReader.hpp>
+#include <clp_ffi_js/ir/StreamReaderDataContext.hpp>
+
+namespace clp_ffi_js::ir {
+using schema_tree_node_id_t = std::optional<clp::ffi::SchemaTree::Node::id_t>;
+using StructuredLogEvent = clp::ffi::KeyValuePairLogEvent;
+
+/**
+ * Class that implements the `clp::ffi::ir_stream::IrUnitHandlerInterface` to buffer log events and
+ * determine the schema-tree node ID of the timestamp kv-pair.
+ */
+class IrUnitHandler {
+public:
+    /**
+     * @param deserialized_log_events The vector in which to store deserialized log events.
+     * @param log_level_key Key name of schema-tree node that contains the authoritative log level.
+     * @param timestamp_key Key name of schema-tree node that contains the authoritative timestamp.
+     */
+    IrUnitHandler(
+            std::shared_ptr<std::vector<LogEventWithFilterData<StructuredLogEvent>>>
+                    deserialized_log_events,
+            std::string log_level_key,
+            std::string timestamp_key
+    )
+            : m_log_level_key{std::move(log_level_key)},
+              m_timestamp_key{std::move(timestamp_key)},
+              m_deserialized_log_events{std::move(deserialized_log_events)} {}
+
+    // Methods implementing `clp::ffi::ir_stream::IrUnitHandlerInterface`.
+    /**
+     * Buffers the log event.
+     * @param log_event
+     * @return IRErrorCode::IRErrorCode_Success
+     */
+    [[nodiscard]] auto handle_log_event(StructuredLogEvent&& log_event
+    ) -> clp::ffi::ir_stream::IRErrorCode;
+
+    /**
+     * @param utc_offset_old
+     * @param utc_offset_new
+     * @return IRErrorCode::IRErrorCode_Success
+     */
+    [[nodiscard]] static auto handle_utc_offset_change(
+            [[maybe_unused]] clp::UtcOffset utc_offset_old,
+            [[maybe_unused]] clp::UtcOffset utc_offset_new
+    ) -> clp::ffi::ir_stream::IRErrorCode {
+        SPDLOG_WARN("UTC offset change packets aren't handled currently.");
+
+        return clp::ffi::ir_stream::IRErrorCode::IRErrorCode_Success;
+    }
+
+    /**
+     * Saves the node's ID if it corresponds to events' authoritative timestamp kv-pair.
+     * @param schema_tree_node_locator
+     * @return IRErrorCode::IRErrorCode_Success
+     */
+    [[nodiscard]] auto handle_schema_tree_node_insertion(
+            clp::ffi::SchemaTree::NodeLocator schema_tree_node_locator
+    ) -> clp::ffi::ir_stream::IRErrorCode;
+
+    /**
+     * @return IRErrorCode::IRErrorCode_Success
+     */
+    [[nodiscard]] static auto handle_end_of_stream() -> clp::ffi::ir_stream::IRErrorCode {
+        return clp::ffi::ir_stream::IRErrorCode::IRErrorCode_Success;
+    }
+
+    // Methods
+
+    /**
+     * @return The schema-tree node ID associated with events' authoritative timestamp key.
+     */
+    [[nodiscard]] auto get_timestamp_node_id() const -> schema_tree_node_id_t {
+        return m_timestamp_node_id;
+    }
+
+private:
+    // Methods
+
+     /**
+     * @param id_value_pairs
+     * @return Timestamp from `StructuredLogEvent`
+     */
+    [[nodiscard]] auto get_log_level(StructuredLogEvent::NodeIdValuePairs const& id_value_pairs) const -> LogLevel;
+
+    /**
+     * @param id_value_pairs
+     * @return Timestamp from `StructuredLogEvent`
+     */
+    [[nodiscard]] auto get_timestamp(StructuredLogEvent::NodeIdValuePairs const& id_value_pairs) const -> clp::ffi::value_int_t;
+
+
+    // Variables
+    std::string m_log_level_key;
+    std::string m_timestamp_key;
+
+    clp::ffi::SchemaTree::Node::id_t m_current_node_id{clp::ffi::SchemaTree::cRootId};
+
+    schema_tree_node_id_t m_timestamp_node_id;
+    schema_tree_node_id_t m_log_level_node_id;
+
+    // TODO: Technically, we don't need to use a `shared_ptr` since the parent stream reader will
+    // have a longer lifetime than this class. Instead, we could use `gsl::not_null` once we add
+    // `gsl` into the project.
+    std::shared_ptr<std::vector<LogEventWithFilterData<StructuredLogEvent>>>
+            m_deserialized_log_events;
+};
+}  // namespace clp_ffi_js::ir
+
+#endif  // CLP_FFI_JS_IR_STRUCTUREDIRSTREAMREADER_HPP
