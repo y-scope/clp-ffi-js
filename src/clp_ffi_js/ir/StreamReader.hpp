2aabf7b6469f6b9d3adf3d4938344904d99db010
davemarco <83603688+davemarco@users.noreply.github.com>
2024-12-18 09:18:15 -0500

    Update src/clp_ffi_js/ir/StreamReader.hpp
    Co-authored-by: Junhao Liao <junhao@junhao.ca>

--- src/clp_ffi_js/ir/StreamReader.hpp
+++ src/clp_ffi_js/ir/StreamReader.hpp
@@ -138,6 +138,7 @@ protected:
      * @param use_filter
      * @param log_event_to_string
      * @return See `decode_range`.
+     * @throw Propagates `ToStringFunc`'s exception.
      */
     template <typename LogEvent, typename ToStringFunc>
     requires requires(ToStringFunc func, LogEvent const& log_event) {
