122f1cf383ff0c6eaae3ec8a1b761251cfea44bf
Henry <50559854+Henry8192@users.noreply.github.com>
2025-01-15 12:36:36 -0500

    fix lint

--- src/clp_ffi_js/ir/StructuredIrStreamReader.cpp
+++ src/clp_ffi_js/ir/StructuredIrStreamReader.cpp
@@ -151,10 +151,7 @@ auto StructuredIrStreamReader::decode_range(size_t begin_idx, size_t end_idx, bo
 auto StructuredIrStreamReader::get_log_event_idx_by_timestamp(
         clp::ir::epoch_time_ms_t const timestamp
 ) -> LogEventIdxTsType {
-    return generic_get_log_event_idx_by_timestamp(
-            *m_deserialized_log_events,
-            timestamp
-    );
+    return generic_get_log_event_idx_by_timestamp(*m_deserialized_log_events, timestamp);
 }
 
 StructuredIrStreamReader::StructuredIrStreamReader(
