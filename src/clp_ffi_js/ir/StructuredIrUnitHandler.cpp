d9d0c35d68095ab954c678a4a2f0f4ea0ea9ffcd
Junhao Liao <junhao.liao@yscope.com>
2026-02-12 22:56:37 +0800

    use braces for declaration value assignments; copy error instead of referencing - Apply suggestions from code review
    Co-authored-by: Bingran Hu <bingran.hu@yscope.com>

--- src/clp_ffi_js/ir/StructuredIrUnitHandler.cpp
+++ src/clp_ffi_js/ir/StructuredIrUnitHandler.cpp
@@ -72,10 +72,9 @@ auto parse_log_level_from_value(clp::ffi::Value const& value) -> std::optional<L
     }
 
     if (value.is<clp::ffi::FourByteEncodedTextAst>()) {
-        auto const result
-                = value.get_immutable_view<clp::ffi::FourByteEncodedTextAst>().to_string();
+        auto const result{value.get_immutable_view<clp::ffi::FourByteEncodedTextAst>().to_string()};
         if (result.has_error()) {
-            auto const& error{result.error()};
+            auto const error{result.error()};
             SPDLOG_ERROR(
                     "Failed to decode `clp::ffi::FourByteEncodedTextAst`: {} - {}",
                     error.category().name(),
@@ -87,10 +86,9 @@ auto parse_log_level_from_value(clp::ffi::Value const& value) -> std::optional<L
     }
 
     if (value.is<clp::ffi::EightByteEncodedTextAst>()) {
-        auto const result
-                = value.get_immutable_view<clp::ffi::EightByteEncodedTextAst>().to_string();
+        auto const result{value.get_immutable_view<clp::ffi::EightByteEncodedTextAst>().to_string()};
         if (result.has_error()) {
-            auto const& error{result.error()};
+            auto const error{result.error()};
             SPDLOG_ERROR(
                     "Failed to decode `clp::ffi::EightByteEncodedTextAst`: {} - {}",
                     error.category().name(),
