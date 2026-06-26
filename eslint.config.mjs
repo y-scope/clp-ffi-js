2c2b1d6b04d62760b74d49fb347d635c15f21440
Bingran Hu <bingran.hu@yscope.com>
2026-06-26 03:31:05 -0400

    Add special ignored rules for test

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,6 +14,18 @@ const EslintConfig = [
     CommonConfig,
     ...TsConfigArray,
     ...StylisticConfigArray,
+    {
+        files: ["test/**/*.ts"],
+        rules: {
+            "@stylistic/array-element-newline": "off",
+            "dot-notation": "off",
+            "jsdoc/require-description": "off",
+            "jsdoc/require-returns": "off",
+            "max-lines-per-function": "off",
+            "max-statements": "off",
+            "no-magic-numbers": "off",
+        },
+    },
 ];
 
 
